0fe457c64ef74e3891eec1303d28fda01443c769
Piou-piou <pilloud.anthony@gmail.com>
2019-04-12 08:27:42 +0200

    [func] add base files for the login (middleware, store and login page)

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <form action="">
+    <label for="pseudo">Pseudo</label>
+    <input type="text" name="pseudo">
+    <label for="password">Mot de passe</label>
+    <input type="password" name="password">
+  </form>
+</template>
+
+<script>
+  export default {
+    middleware: 'notAuthenticated',
+  }
+</script>
