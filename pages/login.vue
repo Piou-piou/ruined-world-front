3aa4fe35f9fae59b5b64206d0b229e4d485d9391
Piou-piou <pilloud.anthony@gmail.com>
2019-04-12 08:41:59 +0200

    [func] prepares libs for further request [func] add created method in login to test if local storage toekn exist for user. If exist set store and redirect on index page

--- pages/login.vue
+++ pages/login.vue
@@ -8,7 +8,20 @@
 </template>
 
 <script>
+  const Cookie = process.client ? require('js-cookie') : undefined;
+  import jwt from 'jsonwebtoken';
+  import FormData from 'form-data';
+  import fetch from 'node-fetch';
+
   export default {
     middleware: 'notAuthenticated',
+    created() {
+      if (process.client && localStorage.getItem('token') !== null && localStorage.getItem('token') !== '') {
+        this.$store.commit('setAuth', localStorage.getItem('token'));
+        Cookie.set('token', localStorage.getItem('token'));
+
+        this.$router.push('/');
+      }
+    }
   }
 </script>
