a943334e8147b92e9f0903128ace2e5c0960af17
Piou-piou <pilloud.anthony@gmail.com>
2019-04-12 14:11:50 +0200

    [del] console.log [func] add babel for import es6

--- store/index.js
+++ store/index.js
@@ -14,7 +14,6 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit({ commit }, { req }) {
-        console.log('df');
 
         let auth = null;
         if (req.headers.cookie) {
