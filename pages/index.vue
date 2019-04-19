114eb321a95c0fff5330fb2baeebc4decbe1cde9
Piou-piou <pilloud.anthony@gmail.com>
2019-04-19 08:31:27 +0200

    [func] add logout method

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     index de la base
+
+    <button class="logout" @click="logout">
+      Se déconnecter
+    </button>
   </div>
 </template>
 
 <script>
   export default {
     middleware: 'authenticated',
+    methods: {
+      logout() {
+        this.$router.push('/logout');
+      }
+    },
+    created() {
+
+    }
   }
 </script>
