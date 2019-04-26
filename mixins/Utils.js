9842c3a180bc16cc39a21acc1eacc7bb80b8a866
Piou-piou <pilloud.anthony@gmail.com>
2019-04-26 08:23:06 +0200

    [del] old system with store to stock auth token [func] add methods in Utils to get and set auth token

--- mixins/Utils.js
+++ mixins/Utils.js
@@ -1,26 +1,43 @@
-const Cookie = process.client ? require('js-cookie') : undefined;
 import jwt from 'jsonwebtoken';
 import RibsApi from 'ribs-api';
 
 export default {
   methods: {
+    /**
+     * method to get user auth token
+     * @returns {any}
+     */
+    getToken() {
+      return localStorage.token;
+    },
+
+    /**
+     * method to set user auth token
+     * @param value
+     */
+    setToken(value) {
+      localStorage.setItem('token', value);
+    },
+
+    /**
+     * method to test if token of user is always valid
+     * @param page
+     * @returns {boolean}
+     */
     testAndUpdateToken(page = null) {
       if (process.client && localStorage.getItem('token') !== null && localStorage.getItem('token') !== '') {
-        this.$store.commit('setAuth', localStorage.getItem('token'));
-        Cookie.set('token', localStorage.getItem('token'));
-
         const api = new RibsApi('http://dev.ruined-world-api.anthony-pilloud.fr/api/', 'cors');
 
         const jwtInfos = jwt.sign({
-          token: this.$store.state.token,
+          token: this.getToken(),
           iat: Math.floor(Date.now() / 1000) - 30
-        }, this.$store.state.token);
+        }, this.getToken());
 
         const context = this;
 
-        api.post('users/test-token', {'infos': jwtInfos, 'token': this.$store.state.token}).then((data) => {
+        api.post('users/test-token', {'infos': jwtInfos, 'token': this.getToken()}).then((data) => {
           if (data.success === true) {
-            localStorage.setItem('token', data.token);
+            this.setToken(data.token);
 
             if (page === 'login') {
               context.router.push('/');
