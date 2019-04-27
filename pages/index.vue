41468ef665342e5ba0f7f50ccf62eaaad0de379f
Piou-piou <pilloud.anthony@gmail.com>
2019-04-27 22:25:59 +0200

    [func] prepare method to get Current base [func] if guid current base not set, set it first and after get current base

--- pages/index.vue
+++ pages/index.vue
@@ -20,6 +20,39 @@
     },
     created() {
       this.testAndUpdateToken();
+      
+      if (process.client) {
+        if (this.getGuidBase() === null) {
+          const jwtInfos = this.getJwt().sign({
+            token: this.getToken(),
+            iat: Math.floor(Date.now() / 1000) - 30,
+          }, this.getToken());
+
+          this.getApi().post('main-base/', {
+            'infos': jwtInfos,
+            'token': this.getToken()
+          }).then(data => {
+            if (data.success === true) {
+              this.setGuidBase(data.guid_base);
+			} else {
+              this.$router.push('/logout');
+			}
+          });
+		}
+        
+        const jwtInfos = this.getJwt().sign({
+          token: this.getToken(),
+          iat: Math.floor(Date.now() / 1000) - 30,
+          'guid_base': this.getGuidBase(),
+        }, this.getToken());
+
+        this.getApi().post('base/', {
+          'infos': jwtInfos,
+          'token': this.getToken(),
+        }).then(data => {
+          console.log(data);
+        });
+	  }
     }
   }
 </script>
