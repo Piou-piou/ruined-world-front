5592da6c06a3e18ec913b46f705987a35a300dec
Piou-piou <pilloud.anthony@gmail.com>
2019-05-22 22:07:26 +0200

    [func] prepare view of building

--- components/BuildingPopup.vue
+++ components/BuildingPopup.vue
@@ -3,7 +3,7 @@
     <div class="popup" v-bind:class="{displayed: isDisplayed}">
       <div class="content">
         <div id="set-content">
-
+          <h1>{{building.name}}</h1>
         </div>
 
         <div class="link">
@@ -16,13 +16,38 @@
   </div>
 </template>
 <script>
+  import Utils from '~/mixins/Utils';
+
   export default {
+    mixins: [Utils],
     props: {
       'isDisplayed': false
     },
+    data() {
+      return {
+        building: {},
+        construction_time : null,
+        resources_build: {}
+      }
+    },
     methods: {
       getBuilding(array_name) {
+        const jwtInfos = this.getJwt().sign({
+          token: this.getToken(),
+          iat: Math.floor(Date.now() / 1000) - 30,
+          guid_base: this.getGuidBase(),
+          array_name
+        }, this.getToken());
 
+        this.getApi().post('/buildings/show/', {
+          'infos': jwtInfos,
+          'token': this.getToken()
+        }).then(data => {
+          console.log('df');
+          this.building = data.building;
+          this.construction_time = data.construction_time;
+          this.resources_build = data.resources_build;
+        });
       }
     }
   }
