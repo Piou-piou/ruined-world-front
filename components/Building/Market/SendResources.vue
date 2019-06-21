ddd1ec5409a4ab76381dd6b34b640b95b93bed69
Piou-piou <pilloud.anthony@gmail.com>
2019-06-21 08:15:41 +0200

    [func] prepare view to send resources

---
+++ components/Building/Market/SendResources.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>Envoyer des ressources à une base en saisissant ses coordonnées</h3>
+    <form action="">
+      x: <input type="text" name="posx" v-model="posx">
+      y: <input type="text" name="posy" v-model="posy">
+      <button  v-on:click.stop.prevent="submit">Envoyer</button>
+    </form>
+  </div>
+</template>
