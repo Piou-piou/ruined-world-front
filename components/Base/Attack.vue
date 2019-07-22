0f64e4d937a8ff4b471ea1eeaa33ddd80549eca9
Piou-piou <pilloud.anthony@gmail.com>
2019-07-22 08:05:58 +0200

    [func] add base informations on attack page

--- components/Base/Attack.vue
+++ components/Base/Attack.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
+    <h1>Evoyer les troupes à {{base.name}}</h1>
 
+    <ul>
+      <li>Propriétaire : {{base.user.pseudo}}</li>
+      <li>Alliance : non dévelopé</li>
+      <li>Base : {{base.name}}</li>
+      <li>Coordonnées : {{base.posx}} / {{base.poxy}}</li>
+      <li>temps de trajet estimé : {{base.travel_time}}</li>
+    </ul>
   </div>
 </template>
 
