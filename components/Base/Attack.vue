f6f93158b150cacefdc777678358ce27729960cd
Piou-piou <pilloud.anthony@gmail.com>
2019-07-22 08:34:28 +0200

    [func] prepare send units to a base method

--- components/Base/Attack.vue
+++ components/Base/Attack.vue
@@ -17,7 +17,7 @@
       <div v-if="Object.keys(units).length > 0">
         <div v-for="(unit, key) in units" :key="key">
           <label for="">{{ unit.name }}</label>
-          <input type="number"> / {{ unit.number }}
+          <input type="number" v-bind:data-arrayname="unit.array_name" class="unit">/ {{ unit.number }}
         </div>
       </div>
       <div v-else>Aucune unité présente dans la base</div>
@@ -38,6 +38,31 @@
       return {
         base: this.$parent.base,
         units: {}
+      }
+    },
+    methods: {
+      /**
+       * method to send units to the fight simulator and get results of it
+       */
+      submit() {
+        const units = {};
+        const inputs = document.getElementsByClassName('unit');
+
+        Array.from(inputs).forEach((element, index) => {
+          if (element.value && element.value > 0) {
+            units[element.dataset.arrayname] = element.value;
+          }
+        });
+
+        this.getApi().post('dont-know', {
+          infos: this.getJwtValues({units}),
+          token: this.getToken(),
+        }).then(data => {
+          this.updateTokenIfExist(data.token);
+          if (data.success) {
+
+          }
+        })
       }
     },
     mounted() {
