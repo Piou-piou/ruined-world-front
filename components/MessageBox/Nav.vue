12c57aa6770e10292923891f29aa9847f4b9b739
Piou-piou <pilloud.anthony@gmail.com>
2019-07-26 07:56:02 +0200

    [func] prepare vues for message

---
+++ components/MessageBox/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link to="/"><button>Index</button></nuxt-link>
+    </div>
+
+    <div>
+      <nuxt-link to="/message-box/sent-messages"><button>Messages envoyés</button></nuxt-link>
+      <nuxt-link to="/message-box/write"><button>Ecrire un message</button></nuxt-link>
+      <nuxt-link to="/message-box/fight-reports"><button>Rapport de combat</button></nuxt-link>
+    </div>
+  </div>
+</template>
---
+++ pages/message-box/write.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Nav/>
+
+    <h1>Ecrire un message</h1>
+  </div>
+</template>
+
+<script>
+  import Utils from '~/mixins/Utils';
+  import Nav from '~/components/MessageBox/Nav.vue';
+
+  export default {
+    mixins: [Utils],
+    components: {
+      Nav
+    },
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+  }
+</script>
