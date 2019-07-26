e6809c0db3e8349f880651b98367cf416bf280b3
Piou-piou <pilloud.anthony@gmail.com>
2019-07-26 11:45:06 +0200

    [edit] viewport for mobile

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'ruined-world',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
       { hid: 'description', name: 'description', content: 'Ruined world, gestion game is a free game around gestion of a base and an army and ressources production' }
     ],
     link: [
