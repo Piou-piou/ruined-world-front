b527ddcfb36fef1afbcf0a10c945b1ddbe46ad03
Piou-piou <pilloud.anthony@gmail.com>
2019-07-28 21:12:49 +0200

    [func] add method to show a message

--- pages/message-box/show-message.vue
+++ pages/message-box/show-message.vue
@@ -2,7 +2,10 @@
   <div>
     <Nav/>
 
-    <h1>Un message</h1>
+    <h1>{{message.subject}}</h1>
+    <div>
+      {{message.message}}
+    </div>
   </div>
 </template>
 
@@ -17,14 +20,26 @@
     },
     data() {
       return {
-
+        message: {}
       };
     },
     methods: {
 
     },
     mounted() {
-
+      if (localStorage.getItem('message') && localStorage.getItem('message') !== '' && localStorage.getItem('message') !== undefined) {
+        this.getApi().post('message/show/', {
+          infos: this.getJwtValues({message_id: localStorage.getItem('message')}),
+          token: this.getToken(),
+        }).then(data => {
+          this.updateTokenIfExist(data.token);
+          if (data.success === true && data.message.length > 0) {
+            this.message = JSON.parse(data.message).message;
+          }
+        });
+      } else {
+        this.$router.push('/message-box');
+      }
     }
   }
 </script>
