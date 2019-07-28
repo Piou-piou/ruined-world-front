fb5d0eca2a314c1c66292d09b87be08293ac972f
Piou-piou <pilloud.anthony@gmail.com>
2019-07-28 21:13:51 +0200

    [func] add link to show index of message box in nav of message box

--- components/MessageBox/Nav.vue
+++ components/MessageBox/Nav.vue
@@ -5,6 +5,7 @@
     </div>
 
     <div>
+      <nuxt-link to="/message-box"><button>Boite de réception</button></nuxt-link>
       <nuxt-link to="/message-box/sent-messages"><button>Messages envoyés</button></nuxt-link>
       <nuxt-link to="/message-box/write"><button>Ecrire un message</button></nuxt-link>
       <nuxt-link to="/message-box/fight-reports"><button>Rapport de combat</button></nuxt-link>
