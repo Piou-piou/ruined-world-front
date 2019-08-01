91303625e216be7b042f8a990bbebf481a2f150f
Piou-piou <pilloud.anthony@gmail.com>
2019-08-01 20:08:48 +0200

    [func] add other reports page

---
+++ pages/message-box/other-reports.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="messages">
+    <Nav/>
+
+    <h1>Autres rapports</h1>
+
+    <ListMessages type="other-report"></ListMessages>
+  </div>
+</template>
+
+<script>
+  import Nav from '~/components/MessageBox/Nav.vue';
+  import ListMessages from '~/components/MessageBox/ListMessages.vue';
+
+  export default {
+    components: {
+      Nav,
+      ListMessages
+    },
+  }
+</script>
