051fd835b7fbcf036aa2196f492420d34a951750
Piou-piou <pilloud.anthony@gmail.com>
2019-08-06 07:44:57 +0200

    [func] add display ranked players in a table

--- pages/ranking.vue
+++ pages/ranking.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-  
+    <div>
+      <nuxt-link to="/"><button>Index</button></nuxt-link>
+    </div>
+    <h1>Classement</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Rang</th>
+          <th>Pseudo</th>
+          <!--<th>Alliance</th>-->
+          <th>Points</th>
+          <!--<th>Bases</th>-->
+        </tr>
+      </thead>
+      <tbody>
+        <tr  v-for="(player, key) in players" :key="key">
+          <td></td>
+          <td>{{player.pseudo}}</td>
+          <td>{{player.points}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -11,22 +33,31 @@
     mixins: [Utils],
     data() {
       return {
-        players: {}
+        players: {},
+        currentPage: null,
+        maxPage: null
       }
     },
     methods: {
-    
+      /**
+       * method to get ranked players
+       */
+      getRankedPlayers() {
+        this.getApi().post('ranking/', {
+          infos: this.getJwtValues({page_number: this.currentPage}),
+          token: this.getToken(),
+        }).then((data) => {
+          this.updateTokenIfExist(data.token);
+          if (data.success) {
+            this.players = data.players;
+            this.currentPage = data.max_pages;
+            this.maxPage = data.max_pages;
+          }
+        });
+      }
     },
     mounted() {
-      this.getApi().post('ranking/', {
-        infos: this.getJwtValues(),
-        token: this.getToken(),
-      }).then((data) => {
-        this.updateTokenIfExist(data.token);
-        if (data.success) {
-          console.log(data);
-        }
-      });
+      this.getRankedPlayers();
     }
   }
 </script>
