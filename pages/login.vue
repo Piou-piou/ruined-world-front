87c49f8923604138e9217b28bb96bec1e2ec80db
Piou-piou <pilloud.anthony@gmail.com>
2019-08-07 08:03:10 +0200

    [func] create visual and fields of sign up form [refa] fields of login form

--- pages/login.vue
+++ pages/login.vue
@@ -6,40 +6,48 @@
       </div>
     </div>
 
-    <div v-bind:class="{active: !loading}">
+    <div v-bind:class="{active: !loading}" id="login-form">
       <form action="">
         <h1>Ruined World</h1>
         <h2>Connexion</h2>
 
         <div class="block">
-          <label for="pseudo">Pseudo</label>
-          <input type="text" name="pseudo" id="pseudo" v-model="pseudo">
+          <label>Pseudo</label>
+          <input type="text" name="pseudo" v-model="pseudo">
         </div>
         <div class="block">
-          <label for="password">Mot de passe</label>
-          <input type="password" name="password" id="password" v-model="password">
+          <label>Mot de passe</label>
+          <input type="password" name="password" v-model="password">
         </div>
         <button type="submit" v-on:click.stop.prevent="submit">Connexion</button>
       </form>
-      <a href="">Tu n'as pas encore de compte ? Inscris toi !</a>
+      <a href="" v-on:click.stop.prevent="changeForm">Tu n'as pas encore de compte ? Inscris toi !</a>
     </div>
 
-    <div>
+    <div id="registration-form">
       <form action="">
         <h1>Ruined World</h1>
-        <h2>Connexion</h2>
+        <h2>Inscription</h2>
 
         <div class="block">
-          <label for="pseudo">Pseudo</label>
-          <input type="text" name="pseudo" id="pseudo" v-model="pseudo">
+          <label>Pseudo</label>
+          <input type="text" name="pseudo" v-model="pseudo">
         </div>
         <div class="block">
-          <label for="password">Mot de passe</label>
-          <input type="password" name="password" id="password" v-model="password">
+          <label>E-mail</label>
+          <input type="email" name="mail" v-model="mail">
         </div>
-        <button type="submit" v-on:click.stop.prevent="submit">Connexion</button>
+        <div class="block">
+          <label>Mot de passe</label>
+          <input type="password" name="password" v-model="password">
+        </div>
+        <div class="block">
+          <label>Vérifies ton Mot de passe</label>
+          <input type="password" name="verify-password" v-model="verifyPassword">
+        </div>
+        <button type="submit" v-on:click.stop.prevent="submit">Inscription</button>
       </form>
-      <a href="">Retour</a>
+      <a href="" v-on:click.stop.prevent="changeForm">Retour</a>
     </div>
   </div>
 </template>
@@ -52,7 +60,9 @@
     data() {
       return {
         pseudo: null,
+        mail: null,
         password: null,
+        verifyPassword: null,
         loading: true
       }
     },
@@ -82,6 +92,17 @@
             return;
           });
       },
+
+      /**
+       * method used to change between login and registration form
+       */
+      changeForm() {
+        const loginForm = document.getElementById('login-form');
+        const registrationForm = document.getElementById('registration-form');
+
+        loginForm.classList.toggle('active');
+        registrationForm.classList.toggle('active');
+      }
     },
     mounted() {
       const testToken = this.testAndUpdateToken('login');
