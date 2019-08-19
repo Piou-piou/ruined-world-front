13025cd283865c251c098309a48579b637f584e0
Piou-piou <pilloud.anthony@gmail.com>
2019-08-19 07:43:05 +0200

    [func] add test text in premium popup

--- components/PremiumPopup.vue
+++ components/PremiumPopup.vue
@@ -3,6 +3,22 @@
     <div class="ribs-popup" v-bind:class="{'ribs-displayed': isDisplayed}">
       <div class="content">
         <div class="close" @click="$emit('close')" data-close>X</div>
+
+        <h1>Avantages premium</h1>
+
+        <div>
+          <h2>Amélioration construction</h2>
+          <p>Avec ce bonus, recrute pendant un temps un chef des constructions qui pourra ajouter un bâtiment de plus à la file d'attente des constructions.</p>
+
+          <h3>Tarifs</h3>
+          <ul>
+            <li>Pour 3 jours : 5 tougnou <button>Acheter</button></li>
+            <li>Pour 15 jours : 20 tougnou <button>Acheter</button></li>
+            <li>Pour 30 jours : 30 tougnou <button>Acheter</button></li>
+          </ul>
+
+          <hr>
+        </div>
       </div>
     </div>
   </div>
