7c2a9c7fffd957b32814dd3e95baae3c7754f273
Piou-piou <pilloud.anthony@gmail.com>
2019-09-28 19:27:30 +0200

    [edit] nav and base was put in header [refa] change nav var name in scss [edit] nav buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,42 @@
 <template>
   <div>
-    <nav class="ribs-container-fluid">
+    <header class="ribs-container-fluid">
+      <div class="row">
+        <nav class="top">
+          <nuxt-link to="/map">Carte</nuxt-link>
+          <nuxt-link to="/message-box">Messagerie<span v-if="unreadMessageNumber > 0"> ({{unreadMessageNumber}})</span></nuxt-link>
+          <nuxt-link to="/fight-simulator">Simulateur de combat</nuxt-link>
+          <nuxt-link to="/ranking">Classement</nuxt-link>
+          <button @click="displayPremiumPopup()">Premium</button>
+        </nav>
+      </div>
       <div class="row">
         <div id="base" class="cxs-12 cmd-1">{{ base.name }}</div>
         <div id="resources" class="cs-12 cmd-8">
           <ul>
             <li><strong>Électricité</strong> : <span
-              :class="{'resources-error': base.resources.electricity === resourcesInfos.max_storage_wharehouse}">
+                    :class="{'resources-error': base.resources.electricity === resourcesInfos.max_storage_wharehouse}">
           {{ base.resources.electricity }}</span> (+{{ resourcesInfos.electricity_production }})
               <span v-if="Object.keys(premiumStorage).length" class="premium"> ({{premiumStorage.electricity}}h)</span>
             </li>
             <li><strong>Fer</strong> : <span
-              :class="{'resources-error': base.resources.iron === resourcesInfos.max_storage_wharehouse}">
+                    :class="{'resources-error': base.resources.iron === resourcesInfos.max_storage_wharehouse}">
           {{ base.resources.iron }}</span> (+{{ resourcesInfos.iron_production }})
               <span v-if="Object.keys(premiumStorage).length" class="premium"> ({{premiumStorage.iron}}h)</span>
             </li>
             <li><strong>Fuel</strong> : <span
-              :class="{'resources-error': base.resources.fuel === resourcesInfos.max_storage_wharehouse}">
+                    :class="{'resources-error': base.resources.fuel === resourcesInfos.max_storage_wharehouse}">
           {{ base.resources.fuel }}</span> (+{{ resourcesInfos.fuel_production }})
               <span v-if="Object.keys(premiumStorage).length" class="premium"> ({{premiumStorage.fuel}}h)</span>
             </li>
             <li><strong>Eau</strong> : <span
-              :class="{'resources-error': base.resources.water === resourcesInfos.max_storage_wharehouse}">
+                    :class="{'resources-error': base.resources.water === resourcesInfos.max_storage_wharehouse}">
           {{ base.resources.water }}</span> (+{{ resourcesInfos.water_production }})
               <span v-if="Object.keys(premiumStorage).length" class="premium"> ({{premiumStorage.water}}h)</span>
             </li>
             <li><strong>Nourriture</strong> :
               <span
-                :class="{'resources-error': base.resources.food === resourcesInfos.max_storage_garner}">
+                      :class="{'resources-error': base.resources.food === resourcesInfos.max_storage_garner}">
             {{ base.resources.food }} <span v-if="resourcesInfos.food_consumption > 0">({{ resourcesInfos.food_consumption }} {{ resourcesInfos.food_string }})</span>
             <span v-if="Object.keys(premiumStorage).length" class="premium"> ({{premiumStorage.food}}h)</span>
           </span>
@@ -35,6 +44,9 @@
           </ul>
         </div>
       </div>
+    </header>
+
+    <nav class="ribs-container-fluid left">
       <div class="row">
         <div class="cxs-12 cmd-2" id="units">
           <h2>Force militaire</h2>
