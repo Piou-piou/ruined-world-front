a61046ab01af16dbdde07d9d952342fc87867b9a
Piou-piou <pilloud.anthony@gmail.com>
2019-09-29 17:05:48 +0200

    [del] link to index on message box nav

--- components/MessageBox/Nav.vue
+++ components/MessageBox/Nav.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
     <div>
-      <nuxt-link to="/"><button>Index</button></nuxt-link>
-    </div>
-
-    <div>
       <nuxt-link to="/message-box"><button>Boite de réception <span v-if="unreadMessages > 0">({{unreadMessages}})</span></button></nuxt-link>
       <nuxt-link to="/message-box/sent-messages"><button>Messages envoyés</button></nuxt-link>
       <nuxt-link to="/message-box/write"><button>Ecrire un message</button></nuxt-link>
