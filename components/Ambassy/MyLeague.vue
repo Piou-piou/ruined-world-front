4ec60c6dcda8475ee9fa716a258d7b245f1f4431
Piou-piou <pilloud.anthony@gmail.com>
2020-09-28 14:15:14 +0200

    [func] prepare myleague page in ambassy

---
+++ components/Ambassy/MyLeague.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Title</h1>
+
+    <div class="link">
+      <a class="cancel">Button</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Utils from '~/mixins/Utils';
+
+  export default {
+    mixins: [Utils],
+    data() {
+      return {
+        base: this.$parent.base
+      }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  }
+</script>
